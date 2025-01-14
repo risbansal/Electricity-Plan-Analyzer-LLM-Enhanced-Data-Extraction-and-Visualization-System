d7e9272b15a4d65d1dd97aef07a89cc1f7587ead
Rishita <risbansal@gmail.com>
2025-01-14 00:56:48 -0600

    Read, write and visualize working

--- data_processing/src/crew.py
+++ data_processing/src/crew.py
@@ -1,78 +1,66 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 import tools
-from langchain_openai import ChatOpenAI
-import tools.composio_tool
-# from tools.file_reader import file_tool
+from crewai_tools import PDFSearchTool, FileWriterTool
+from pathlib import Path
 
-# from composio_crewai import ComposioToolSet, App
-# from composio import Action, App
 
-# composio_tools = ComposioToolSet().get_tools(apps=[App.FILETOOL])
-# file_open_tool = ComposioToolSet.get_tools(actions=['FILETOOL_OPEN_FILE'])
-# edit_file_tool = composio_toolset.get_tools(actions=['FILETOOL_EDIT_FILE'])
-# write_file_tool = composio_toolset.get_tools(actions=['FILETOOL_WRITE'])
+
+
+SCRIPT_DIR = Path(__file__).parent.joinpath('files')
+pdf_path = str(SCRIPT_DIR / "plans.pdf")
+
+pdf_search_tool = PDFSearchTool(pdf=pdf_path)
+
+    
 
 @CrewBase
 class DataprocessingCrew():
     """data_processing crew"""
-
-    # def __init__(self) -> None:
-
-    #     # Ollama
-    #     self.ollama_llm = ChatOpenAI(
-    #         model="mistral",
-    #         base_url="http://localhost:11434/v1",
-    #         api_key="ollama",  # something random
-    #         temperature=0,
-    #     )
-
     
 
-    # Agent definitions
+ 
+
     @agent
-    def data_collector(self) -> Agent:
+    def pdf_reader(self) -> Agent:
         return Agent(
-            config=self.agents_config['data_collector'],
-            tools=[tools.web_scrape, tools.web_crawl],  # Pass in what tools this agent should have
-            # llm = self.ollama_llm,
+            config=self.agents_config['pdf_reader'],
+            tools=[pdf_search_tool], # add tools here or use `agentstack tools add <tool_name>
             verbose=True,
             
             
         )
 
+
     @agent
-    def data_analyzer(self) -> Agent:
+    def data_cleaner(self) -> Agent:
         return Agent(
-            config=self.agents_config['data_analyzer'],
+            config=self.agents_config['data_cleaner'],
             tools=[], # add tools here or use `agentstack tools add <tool_name>
             verbose=True,
+            
+            
         )
 
-    # @agent
-    # def data_writer(self) -> Agent:
-    #     return Agent(
-    #         config=self.agents_config['data_writer'],
-    #         tools=[tools.composio_tool], # add tools here or use `agentstack tools add <tool_name>
-    #         verbose=True,
-    #     )
+    @agent
+    def data_writer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['data_writer'],
+            tools=[FileWriterTool()], # add tools here or use `agentstack tools add <tool_name>
+            verbose=True,
+            
+        )
         
+        
+
     
 
-   
-   
     @task
-    def get_data(self) -> Task:
+    def read_pdf(self) -> Task:
         return Task(
-            config=self.tasks_config['get_data'],
+            config=self.tasks_config['read_pdf'],
         )
-  
-    # @task
-    # def read_efl(self) -> Task:
-    #     return Task(
-    #         config=self.tasks_config['read_efl'],
-    #     )
-
+        
     @task
     def get_relevant_data(self) -> Task:
         return Task(
@@ -81,11 +69,14 @@ class DataprocessingCrew():
 
    
 
-    # @task
-    # def write_to_file(self) -> Task:
-    #     return Task(
-    #         config=self.tasks_config['write_to_file'],
-    #     )
+    @task
+    def write_to_file(self) -> Task:
+        return Task(
+            config=self.tasks_config['write_to_file'],
+        )
+
+
+   
 
     @crew
     def crew(self) -> Crew:
